b03853a32b09d806f2385b361d558bc81bb3f0a5
abdurwebdev <iabdurrehman22.0@gmail.com>
2025-05-28 20:04:06 +0500

    Added

--- server.js
+++ server.js
@@ -1,24 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
-
-dotenv.config();
-
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.use(cors());
-app.use(express.json());
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
 
 app.get('/', (req, res) => {
-  res.send('API is running!');
+  res.send('Hello from Glitch!');
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
